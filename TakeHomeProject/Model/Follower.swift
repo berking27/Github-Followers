4767fde971e54bfd2c67ff17931aa11f789c3f64
berking27 <bekoca@innova.com.tr>
2024-02-14 11:01:20 +0300

    Follower and User Models created

---
+++ TakeHomeProject/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  TakeHomeProject
+//
+//  Created by Berkin KOCA on 14.02.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
