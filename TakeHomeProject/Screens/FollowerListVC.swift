9623657cfecffa90b25e3f82600caaaff78c5bd1
berking27 <bekoca@innova.com.tr>
2024-02-19 17:55:54 +0300

    Paginate next followers when sliding bottom.

--- TakeHomeProject/Screens/FollowerListVC.swift
+++ TakeHomeProject/Screens/FollowerListVC.swift
@@ -18,6 +18,8 @@ class FollowerListVC: UIViewController {
     var followers: [Follower] = []
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
+    var page = 1
+    var hasMoreFollowers = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +28,7 @@ class FollowerListVC: UIViewController {
         configureViewController()
         configureDataSource()
         
-        getFollowers()
+        getFollowers(username: username, page: page)
         
     }
     
@@ -43,18 +45,23 @@ class FollowerListVC: UIViewController {
     func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    func getFollowers(username: String, page: Int) {
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             
             guard let self = self else { return }
             
             switch result {
             case .success(let followers):
-                self.followers = followers
+                if followers.count < 60 {
+                    self.hasMoreFollowers = false
+                }
+                
+                self.followers.append(contentsOf: followers)
                 self.updateDate()
                 
             case .failure(let error):
@@ -84,4 +91,21 @@ class FollowerListVC: UIViewController {
         
     }
     
+}
+
+extension FollowerListVC: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers else { return }
+            page += 1
+            getFollowers(username: username, page: page)
+            
+        }
+        
+    }
 }
