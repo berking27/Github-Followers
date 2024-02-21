fd634e366ca7717981a3f6034638ce338bc6b704
berking27 <bekoca@innova.com.tr>
2024-02-21 17:24:43 +0300

    search bar functionality is done.

--- TakeHomeProject/Screens/FollowerListVC.swift
+++ TakeHomeProject/Screens/FollowerListVC.swift
@@ -16,10 +16,12 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
-    var collectionView: UICollectionView!
-    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,7 +56,10 @@ class FollowerListVC: UIViewController {
     func configureSearchController() {
         let searchController = UISearchController()
         searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
         searchController.searchBar.placeholder = "Search for a username"
+        ///When hit search bar collection view fade won't change
+        searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
         
         
@@ -80,7 +85,7 @@ class FollowerListVC: UIViewController {
                     return
                 }
                 
-                self.updateDate()
+                self.updateData(on: self.followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "OK")
@@ -100,7 +105,7 @@ class FollowerListVC: UIViewController {
         })
     }
     
-    func updateDate() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -128,10 +133,18 @@ extension FollowerListVC: UICollectionViewDelegate {
     }
 }
 
-extension FollowerListVC: UISearchResultsUpdating {
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    
     func updateSearchResults(for searchController: UISearchController) {
-        //
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+        
     }
     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
+    }
     
 }
