498ce33566a61a79e1a70055e6b058b4d6b5c6c3
berking27 <bekoca@innova.com.tr>
2024-02-23 15:23:47 +0300

    User Info VC and User Info Header is created.

---
+++ TakeHomeProject/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  TakeHomeProject
+//
+//  Created by Berkin KOCA on 23.02.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
