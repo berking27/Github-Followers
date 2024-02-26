5ae8d0a78ba83be13ef36268e906a946b6f6fd26
berking27 <bekoca@innova.com.tr>
2024-02-26 15:08:30 +0300

    ItemInfoVC Child Classes integrated. UserInfoVC is done for UI.

--- TakeHomeProject/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ TakeHomeProject/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -14,6 +14,16 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
     
+    var user: User!
+    
+    init(user: User!) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +32,7 @@ class GFItemInfoVC: UIViewController {
         configureStackView()
         layoutUI()
 
-    } 
+    }
     
     //MARK: Private Functions
     
