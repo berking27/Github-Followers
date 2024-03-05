d14ee4653356037c4b2d9ee91d328a9b8607f56c
Berkin KOCA <bekoca@innova.com.tr>
2024-03-05 15:56:22 +0300

    Favorites Empty State is fixed. Min Development target changed to iOS 15.0

--- TakeHomeProject/Custom Views/Views/GFEmptyStateView.swift
+++ TakeHomeProject/Custom Views/Views/GFEmptyStateView.swift
@@ -47,7 +47,7 @@ class GFEmptyStateView: UIView {
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 60)
+            logoImageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 60)
            ])
     }
 
